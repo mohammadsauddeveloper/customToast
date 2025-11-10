9ba2349bc32f0625bdb0cb62d45846a2e9f442a2
DESKTOP-3FBSC30\HP <msaud.zeesofttech@outlook.com>
2025-11-10 12:44:35 +0500

    Update: User can add toast custom background color

--- CustomToast/src/main/java/com/example/customtoast/CustomToast.kt
+++ CustomToast/src/main/java/com/example/customtoast/CustomToast.kt
@@ -5,13 +5,16 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 object CustomToast {
     private var toast: Toast?=null
-    fun Context.showToast(message: String){
+    fun Context.showToast(message: String,color: Int){
         val view = LayoutInflater.from(this).inflate(R.layout.custom_toast,null)
         val text = view.findViewById<TextView>(R.id.tvToast)
+        val card = view.findViewById<CardView>(R.id.card)
         text.text = message
+        card.setBackgroundColor(color)
         toast?.cancel()
         toast = Toast(this)
         toast?.duration = Toast.LENGTH_SHORT
