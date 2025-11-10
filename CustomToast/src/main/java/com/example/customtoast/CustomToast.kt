f446d3fdae2a49db694bd95204729821fc9c7b10
DESKTOP-3FBSC30\HP <msaud.zeesofttech@outlook.com>
2025-11-10 12:27:47 +0500

    Custom Designing card toast

--- CustomToast/src/main/java/com/example/customtoast/CustomToast.kt
+++ CustomToast/src/main/java/com/example/customtoast/CustomToast.kt
@@ -1,13 +1,23 @@
 package com.example.customtoast
 
 import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.TextView
 import android.widget.Toast
 
 object CustomToast {
     private var toast: Toast?=null
     fun Context.showToast(message: String){
+        val view = LayoutInflater.from(this).inflate(R.layout.custom_toast,null)
+        val text = view.findViewById<TextView>(R.id.tvToast)
+        text.text = message
         toast?.cancel()
-        toast = Toast.makeText(this, message, android.widget.Toast.LENGTH_SHORT)
+        toast = Toast(this)
+        toast?.duration = Toast.LENGTH_SHORT
+        toast?.view = view
+        toast?.setGravity(Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL, 0,100)
         toast?.show()
+
     }
 }
